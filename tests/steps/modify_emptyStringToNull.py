13644233add1af50354b554ca2b47e1b7aeb0a63
vamsi1067 <101466711+vamsi1067@users.noreply.github.com>
2022-10-18 20:43:38 +0530

    Update modify_emptyStringToNull.py

--- tests/steps/modify_emptyStringToNull.py
+++ tests/steps/modify_emptyStringToNull.py
@@ -13,9 +13,9 @@ def when_emptyString(context, userId,ssId,resource_seq,entl_seq,cert_ind,job_des
         if item == "empty":
             item.replace("empty", " ")
     return item
-@then('validate empty string field is updated with NV')
-def validatePreProcessing(context):
+@then('validate empty string field is updated with NV userId {userId} ssId {ssId} {resource_seq} {entl_seq} {cert_ind} {job_desc}')
+def validatePreProcessing(context,userId,ssId,resource_seq,entl_seq,cert_ind,job_desc):
     df = pd.read_excel(r'/Users/vamsikrishnareddykapa/Desktop/Project/GitRepos/bdd/tests/steps/data_set.xlsx', engine='openpyxl')
     print("dataFrame: "+ str(df))
-    NA=to_str_replace_null(df,when_emptyString)
+    NA=to_str_replace_null(df,when_emptyString(context,userId,ssId,resource_seq,entl_seq,cert_ind,job_desc))
     print(NA)
